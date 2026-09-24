66e74da24ffbc7f4ca47f5090d86f5bfa5752299
bregueda <yasmin.breguedo@estudante.ifms.edu.br>
2026-09-24 19:33:26 -0400

    questao 6

--- att.js
+++ att.js
@@ -5,43 +5,79 @@ const app = express();
 app.use(express.json());
 
 const tarefas = [
-    
+
       { id: 5, titulo: "Estudar PTAS", concluida: false },
       { id: 6, titulo: "Prova de portugues", concluida: true },
       { id: 7, titulo: "Atividade de quimica", concluida: false }
-  ];
+];
 
-app.get("/", (req, res) => {
-  res.send("API de Tarefas no ar");
-});
 
-app.get("/tarefas", (req, res) => {
-  res.json(tarefas);
-});
 
-app.post("/tarefas", (req, res) => {
+function autenticar(req, res, next) {
+
+  console.log("Autenticação realizada.");
+
+  next();
+}
+
+
+
+function validarTarefa(req, res, next) {
 
   const { titulo } = req.body;
 
   if (!titulo) {
+
     return res.status(400).json({
       erro: "O título é obrigatório."
     });
+
   }
 
-  const novaTarefa = {
-    id: tarefas.length + 5,
-    titulo: titulo,
-    concluida: false
-  };
+  next();
+}
 
-  tarefas.push(novaTarefa);
 
-  return res.status(201).json(novaTarefa);
-});
 
-const PORTA = 3000;
 
-app.listen(PORTA, () => {
-  console.log(`Servidor rodando em http://localhost:${PORTA}`);
+function registrarLog(req, res, next) {
+
+  console.log(
+    `Tarefa sendo criada: ${req.body.titulo}`
+  );
+
+  next();
+}
+
+
+
+
+app.post(
+  "/tarefas",
+  [autenticar, validarTarefa, registrarLog],
+  (req, res) => {
+
+    const novaTarefa = {
+      id: tarefas.length + 5,
+      titulo: req.body.titulo,
+      concluida: false
+    };
+
+    tarefas.push(novaTarefa);
+
+    res.status(201).json({
+      mensagem: "Tarefa criada com sucesso",
+      tarefa: novaTarefa
+    });
+
+  }
+);
+
+
+app.listen(3000, () => {
+
+  console.log(
+    "Servidor rodando em http://localhost:3000"
+  );
+
 });
