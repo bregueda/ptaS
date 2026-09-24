cf73b3fcf485be2e190ccd399bf9c63347e40cf7
bregueda <yasmin.breguedo@estudante.ifms.edu.br>
2026-09-24 19:31:17 -0400

    criar uma nova tarefa

--- att.js
+++ att.js
@@ -2,40 +2,42 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const tarefas = [
     
-     { id: 5, titulo: "Estudar PTAS", concluida: false },
-     { id: 6, titulo: "Prova de portugues", concluida: true },
-     { id: 7, titulo: "Atividade de quimica", concluida: false }
- ];
-
-function filtrarTarefas(req, res, next) {
-
-  const { concluida } = req.query;
-
-  if (concluida !== undefined) {
-
-    const statusBuscado = concluida === "true";
-
-    req.filtrarTarefas = tarefas.filter(
-      t => t.concluida === statusBuscado
-    );
-
-  } else {
-
-    req.filtrarTarefas = tarefas;
-
-  }
-
-  next();
-}
+      { id: 5, titulo: "Estudar PTAS", concluida: false },
+      { id: 6, titulo: "Prova de portugues", concluida: true },
+      { id: 7, titulo: "Atividade de quimica", concluida: false }
+  ];
 
 app.get("/", (req, res) => {
   res.send("API de Tarefas no ar");
 });
 
-app.get("/tarefas", filtrarTarefas, (req, res) => {
-  res.json(req.filtrarTarefas);
+app.get("/tarefas", (req, res) => {
+  res.json(tarefas);
+});
+
+app.post("/tarefas", (req, res) => {
+
+  const { titulo } = req.body;
+
+  if (!titulo) {
+    return res.status(400).json({
+      erro: "O título é obrigatório."
+    });
+  }
+
+  const novaTarefa = {
+    id: tarefas.length + 5,
+    titulo: titulo,
+    concluida: false
+  };
+
+  tarefas.push(novaTarefa);
+
+  return res.status(201).json(novaTarefa);
 });
 
 const PORTA = 3000;
